8c0401dd3db203b62c1e9bc71848e93c9f1db92f
Floris Bernard <floris.bernard@gmail.com>
2019-01-13 15:56:28 +0100

    Fix command name in CLI usage

--- helpToReadme.js
+++ helpToReadme.js
@@ -1,16 +1,19 @@
-const { fork } = require('child_process');
-const fs = require('fs-extra');
+const { fork } = require("child_process");
+const fs = require("fs-extra");
 
-const help = fork('./index', ['help'], { silent: true });
-let output = '';
+const help = fork("./cli", ["help"], { silent: true });
+let output = "";
 
-help.stdout.on('data', (data) => {
+help.stdout.on("data", data => {
   output += data;
 });
 
-help.on('close', () => {
-  const readmeContents = fs.readFileSync('./readme.md', { encoding: 'utf8' });
-  const newContents = readmeContents.replace(/```\s+index[\s\S]+?```/, `\`\`\`\n${output.trim()}\n\`\`\``);
+help.on("close", () => {
+  const readmeContents = fs.readFileSync("./readme.md", { encoding: "utf8" });
+  const newContents = readmeContents.replace(
+    /```\s+discogs-data-tools[\s\S]+?```/,
+    `\`\`\`\n${output.trim().replace(/cli/g, "discogs-data-tools")}\n\`\`\``
+  );
 
-  fs.writeFileSync('./readme.md', newContents, { encoding: 'utf8' });
+  fs.writeFileSync("./readme.md", newContents, { encoding: "utf8" });
 });
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@ data dumps see: https://data.discogs.com/
 
 ## Installation
 ```
-npm install discogs-data-tools
+npm install -g discogs-data-tools
 ```
 Please note that some dependencies are built using `node-gyp`, which has additional prerequisites.
 See the [node-gyp documentation](https://github.com/nodejs/node-gyp#Installation) for more info.
@@ -17,11 +17,11 @@ See the [node-gyp documentation](https://github.com/nodejs/node-gyp#Installation
 
 <!-- below section is automatically generated. Do not modify -->
 ```
-index <command> [...args]
-For help on a command run "index <command> help"
+discogs-data-tools <command> [...args]
+For help on a command run "discogs-data-tools <command> help"
 
 Commands:
-  index fetch  fetch data dump from discogs
+  discogs-data-tools fetch  fetch data dump from discogs
 
 Options:
   --version  Show version number                                       [boolean]
