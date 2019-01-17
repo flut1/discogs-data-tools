26eed34054c0ec3d503a79029d61da790ae68dcc
Floris Bernard <floris@mediamonks.com>
2019-01-17 18:21:28 +0100

    Check if dump exists in verify command

--- commands/verify.js
+++ commands/verify.js
@@ -1,6 +1,16 @@
 const { verify } = require("../verify");
+const localDumps = require("../localDumps");
 
 module.exports = function(argv) {
+  const dumpFiles = localDumps.findData(argv["dump-version"], argv.types, argv['data-dir']);
+
+  for (let i = 0; i < dumpFiles.length; i++) {
+    if (!dumpFiles[i]) {
+      console.error(`No dump file found for ${argv["dump-version"]} ${argv.types[i]}`);
+      return;
+    }
+  }
+
   verify(argv["dump-version"], argv["types"], argv["data-dir"]).catch(e =>
     console.error(e)
   );
